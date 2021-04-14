02a09273dcc8f68fa17ea75758a36b8f4709505d
nickzanieri <niccolo.zanieri.13@gmail.com>
2021-04-14 18:45:32 +0200

    Added Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length= 100)
+    description = models.TextField(max_length= 150, blank= True)
+    price = models.DecimalField(max_digits= 15, decimal_places= 2)
+    summary = models.TextField(max_length= 100, blank= True)
+    featured = models.BooleanField(default= False)
